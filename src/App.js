d70a5c66bc30e75e2c5743876e24bcab9f863d57
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-11-01 15:48:36 -0400

    consolidated some files; removed unrequired one.

--- src/App.js
+++ src/App.js
@@ -4,32 +4,30 @@ import fieldsetsRequirements from "./data/intake-form.json";
 const fieldsetsData = Object.assign({}, fieldsetsRequirements.fieldsets);
 
 function App(props) {
-  return (
-    <React.Fragment>
-      <header>
-        <div>
-          <h1>Intake Form</h1>
-        </div>
-      </header>
-      <main>
-        <div>
-          <ul>
-            {
-              Object.keys(fieldsetsData).map(k => (
-                <li key={k}><a href={`/${k}`}>{fieldsetsData[k].legend}</a></li>
-              ))
-            }
-          </ul>
-          {props.children}
-        </div>
-      </main>
-      <footer>
-        <div>
-          <p>&copy; 2022 Edlc</p>
-        </div>
-      </footer>
-    </React.Fragment>
-  );
+  return (<React.Fragment>
+    <header>
+      <div>
+        <h1>Intake Form</h1>
+      </div>
+    </header>
+    <main>
+      <div>
+        <ul>
+          {
+            Object.keys(fieldsetsData).map(k => (
+              <li key={k}><a href={`/${k}`}>{fieldsetsData[k].legend}</a></li>
+            ))
+          }
+        </ul>
+        {props.children}
+      </div>
+    </main>
+    <footer>
+      <div>
+        <p>&copy; 2022 Edlc</p>
+      </div>
+    </footer>
+  </React.Fragment>)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-
 import App from './App';
-import './index.css';
+
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+// } from "react-router-dom";
+
+console.log('here');
+/*import './css/index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-]);
+]);*/
 //
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
